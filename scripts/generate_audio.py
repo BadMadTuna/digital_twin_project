f488dc2dcef4ece5e7d1b8f530ca9897a80bd5ed
BadMadTuna <n.behl@gmx.de>
2025-12-11 16:20:45 +0100

    modified generate_audio.py

--- scripts/generate_audio.py
+++ scripts/generate_audio.py
@@ -10,9 +10,9 @@ from TTS.tts.layers.xtts.tokenizer import VoiceBpeTokenizer
 
 # --------------------------------------------------------------------------
 # --- CONFIGURATION ---
-MODEL_DIR = "/home/ubuntu/digital_twin_project/models/xtts_finetuned-December-11-2025_01+15PM-3e81100"
+MODEL_DIR = "/home/ubuntu/digital_twin_project/models/xtts_finetuned-December-11-2025_02+59PM-bae2302"
 # Use the checkpoint you verified exists
-MODEL_CHECKPOINT_NAME = "checkpoint_3070.pth" 
+MODEL_CHECKPOINT_NAME = "checkpoint_5000.pth" 
 MODEL_CHECKPOINT_PATH = os.path.join(MODEL_DIR, MODEL_CHECKPOINT_NAME)
 CONFIG_PATH = os.path.join(MODEL_DIR, "config.json")
 
